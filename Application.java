e918af4673f626e4147c490670e5e794ad246834
Jakub Zuber <greengo_from_poland@yahoo.com>
2016-11-07 01:05:01 +0100

    Core files for the Apple RSS Feeds - Top 10 free Apps
    Files up to date (7th of november 2016)

---
+++ Application.java
@@ -0,0 +1,51 @@
+package eu.writtenin.top10downloader;
+
+/**
+ * Created by Kuba on 2016-11-06.
+ */
+
+public class Application {
+    private String name;
+    private String artist;
+    private String releaseDate;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + "\n" +
+                "Artist: " + getArtist() + "\n" +
+                "Release Date: " + getReleaseDate() + "\n";
+    }
+}
